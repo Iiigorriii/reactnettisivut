09c5df145c502ec3a2aecdaa2183b27a68556266
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-06 19:12:07 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
 import { Layout } from "../components/Layout";
 
-const Home = () => <Layout></Layout>;
+const Home = () => (
+  <Layout>
+    <a href="https://github.com/Iiigorriii/reactnettisivut" target="_blank">
+      GitHub
+    </a>
+  </Layout>
+);
 
 export default Home;
