1f41d0a9bffb9dd83391dc2643766e071bfd386f
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-10 17:57:58 +0300

    OneRepMaxCalc1.0

--- pages/onerepmaxcalc/index.js
+++ pages/onerepmaxcalc/index.js
@@ -1,9 +1,50 @@
+import { useState } from "react";
 import { Layout } from "../../components/Layout";
 
-const oneRepMax = () => (
-  <Layout>
-    <h1> Tulossa pian!</h1>
-  </Layout>
-);
+import styles from "../../styles/OneRepMax.module.css";
+
+const oneRepMax = () => {
+  const [Lift, setLift] = useState("0");
+  const [Repetitions, setRepetitions] = useState("0");
+  const [answer, setAnswer] = useState("");
+
+  const calculate = () => {
+    return Number(Lift / (1.0278 - 0.0278 * Repetitions)).toFixed(1);
+  };
+
+  const onClick = () => {
+    const result = calculate();
+    setAnswer(result);
+  };
+
+  return (
+    <Layout>
+      <div className={styles.laskuri}>
+        <div className={styles.label}>
+          <label htmlFor="Lift">Lift</label>
+          <input
+            type="number"
+            id="Lift"
+            value={Lift}
+            onChange={(e) => setLift(e.target.value)}
+          />
+        </div>
+        <div className={styles.label}>
+          <label htmlFor="Repetitions">Repetitions</label>
+          <input
+            type="number"
+            id="Repetitions"
+            value={Repetitions}
+            onChange={(e) => setRepetitions(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={() => onClick()}>Laske</button>
+        </div>
+        <h2>One rep max: {answer}</h2>
+      </div>
+    </Layout>
+  );
+};
 
 export default oneRepMax;
