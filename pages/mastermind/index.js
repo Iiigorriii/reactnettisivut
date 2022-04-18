6d3ce75797be2886457f8378d0480c07695257c3
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-18 12:26:56 +0300

    Update index.js

--- pages/mastermind/index.js
+++ pages/mastermind/index.js
@@ -1,5 +1,12 @@
+import React from "react";
 import { Layout } from "../../components/Layout";
 
-const MasterMind = () => {};
+const MasterMind = () => {
+  return (
+    <Layout>
+      <h1>Tulossa pian!</h1>
+    </Layout>
+  );
+};
 
 export default MasterMind;
