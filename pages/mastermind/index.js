16bc52239d44a6669d23ddc36d3cb9877872662a
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-18 13:14:28 +0300

    MasterMind coding

--- pages/mastermind/index.js
+++ pages/mastermind/index.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { Layout } from "../../components/Layout";
 
+import styles from "../../styles/MasterMind.module.css";
+
 const MasterMind = () => {
   return (
     <Layout>
-      <h1>Tulossa pian!</h1>
+      <div class="content">
+        <div class="fullBoard">
+          <div class="board"></div>
+          <div class="pegs"></div>
+        </div>
+        <div class="other">
+          <div class="code">
+            <div class="secretColor" id="secretColor1"></div>
+            <div class="secretColor" id="secretColor2"></div>
+            <div class="secretColor" id="secretColor3"></div>
+            <div class="secretColor" id="secretColor4"></div>
+          </div>
+          <div class="colorBoard">
+            <div class="color" id="blue"></div>
+            <div class="color" id="green"></div>
+            <div class="color" id="red"></div>
+            <div class="color" id="yellow"></div>
+            <div class="color" id="orange"></div>
+          </div>
+          <div class="currentColor"></div>
+          <div class="submit">Submit</div>
+        </div>
+      </div>
     </Layout>
   );
 };
