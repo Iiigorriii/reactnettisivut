ed962fb865c1d46dcf92d06ce9375889a29dae63
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-18 21:03:46 +0300

    Update index.js

--- pages/hangman/index.js
+++ pages/hangman/index.js
@@ -1,10 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import { Layout } from "../../components/Layout";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const Hangman = () => (
+  <Layout>
+    <h1>testi</h1>
+  </Layout>
 );
+
+export default Hangman;
