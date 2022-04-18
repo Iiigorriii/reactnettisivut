e9da39799db61249fd588b8500124153f1da654b
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-18 21:47:04 +0300

    Update index.js

--- pages/hangman/index.js
+++ pages/hangman/index.js
@@ -1,12 +1,16 @@
 import { Layout } from "../../components/Layout";
 import React from "react";
 import styles from "../../styles/HangMan.module.css";
+import Figure from "../../components/Hangman/Figure";
 
 const Hangman = () => {
   return (
     <Layout>
       <h1 className={styles.h1}>Hangman</h1>
       <p>Find the hidden word - Enter a letter</p>
+      <div className="gameContainer">
+        <Figure />
+      </div>
     </Layout>
   );
 };
