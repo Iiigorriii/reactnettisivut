d512b56f79f4508786ffb4f1d80d463b3d0bb25a
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-19 13:50:44 +0300

    Hangman randomword

--- pages/hangman/index.js
+++ pages/hangman/index.js
@@ -1,10 +1,21 @@
 import { Layout } from "../../components/Layout";
-import React from "react";
+import { useState, useEffect } from "react";
+import { ENGLISH_WORDS } from "../../misc/hangmanimages/words";
 
 const Hangman = () => {
+  const [answer, setAnswer] = useState("");
+  const randomWord = () => {
+    setAnswer(ENGLISH_WORDS[Math.floor(Math.random() * ENGLISH_WORDS.length)]);
+  };
+
+  useEffect(() => {
+    randomWord();
+  }, []);
+
   return (
     <Layout>
-      <p>Hangman game tulossa pian!!</p>
+      <h1>Hangman</h1>
+      <h1>{answer}</h1>
     </Layout>
   );
 };
