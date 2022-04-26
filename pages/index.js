ca44c36154723815553e65fe2051e887c9b1a8af
Iiigorriii <83778498+Iiigorriii@users.noreply.github.com>
2022-04-26 13:12:52 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import { Text } from "@chakra-ui/react";
 import { Layout } from "../components/Layout";
 
 const Home = () => (
   <Layout>
+    <Text fontSize={"2xl"}>Linkki nettisivujen GitHubiin:</Text>
     <a
       href="https://github.com/Iiigorriii/reactnettisivut"
       target="_blank"
