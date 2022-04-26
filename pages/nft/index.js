2595878e511e8abd9562df1f6a45ae1fdb21556f
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 01:57:49 +0300

    Update index.js

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -1,23 +1,23 @@
 import { Heading } from "@chakra-ui/react";
 import { Layout } from "../../components/Layout";
-import { ChakraProvider } from "@chakra-ui/react";
-import { ImageSlide } from "react-chakra-slide-show";
-import React, { useMemo } from "react";
+// import { ChakraProvider } from "@chakra-ui/react";
+// import { ImageSlide } from "react-chakra-slide-show";
+// import React, { useMemo } from "react";
 
 const Nft = () => {
-  const images = useMemo(() => ["Abstract.jpg", "CryptoEagle.jpg"], []);
+  //   const images = useMemo(() => ["Abstract.jpg", "CryptoEagle.jpg"], []);
 
-  const captions = useMemo(
-    () => ["caption for Abstract.jpg", "caption for CryptoEagle.jpg"],
-    []
-  );
+  //   const captions = useMemo(
+  //     () => ["caption for Abstract.jpg", "caption for CryptoEagle.jpg"],
+  //     []
+  //   );
 
   return (
     <Layout>
       <Heading>NFT</Heading>
-      <ChakraProvider>
+      {/* <ChakraProvider>
         <ImageSlide images={images} captions={captions} />
-      </ChakraProvider>
+      </ChakraProvider> */}
     </Layout>
   );
 };
