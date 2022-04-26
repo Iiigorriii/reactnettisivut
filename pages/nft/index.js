d597689d760a1f00ae4a7802c4def4b62133c7c2
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 01:43:39 +0300

    NFT slideshow

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -1,10 +1,23 @@
 import { Heading } from "@chakra-ui/react";
 import { Layout } from "../../components/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
+import { ImageSlide } from "react-chakra-slide-show";
+import React, { useMemo } from "react";
 
 const Nft = () => {
+  const images = useMemo(() => ["Abstract.jpg", "CryptoEagle.jpg"], []);
+
+  const captions = useMemo(
+    () => ["caption for Abstract.jpg", "caption for CryptoEagle.jpg"],
+    []
+  );
+
   return (
     <Layout>
       <Heading>NFT</Heading>
+      <ChakraProvider>
+        <ImageSlide images={images} captions={captions} />
+      </ChakraProvider>
     </Layout>
   );
 };
