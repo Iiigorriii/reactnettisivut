959c0cd1de6666faa237b237cf0d33ec0b8e8756
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 01:17:54 +0300

    Create index.js

---
+++ pages/nft/index.js
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import { Layout } from "../../components/Layout";
+
+const Nft = () => {
+  return (
+    <Layout>
+      <Heading>NFT</Heading>
+    </Layout>
+  );
+};
+
+export default Nft;
