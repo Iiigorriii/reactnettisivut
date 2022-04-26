3bb1b6ae72e3ff672ac20b5d48fa6a15f1fbeb88
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-26 14:51:21 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import { Layout } from "../components/Layout";
 
 const Home = () => (
   <Layout>
-    <Text fontSize={"2xl"}>Linkki nettisivujen GitHubiin:</Text>
     <a
       href="https://github.com/Iiigorriii/reactnettisivut"
       target="_blank"
