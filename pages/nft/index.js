b120b3e6e2c0eebc738e67f9c724c7216b5fcca3
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 22:29:47 +0300

    Update index.js

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -41,7 +41,15 @@ const Nft = ({ video, src, alt }) => (
   <div>
     <Tooltip label={alt} closeDelay={800} placement="top">
       {video ? (
-        <video controls autoPlay loop muted src={src} alt={alt} className={styles.nft} />
+        <video
+          controls
+          autoPlay
+          loop
+          muted
+          src={src}
+          alt={alt}
+          className={styles.nft}
+        />
       ) : (
         <img src={src} alt={alt} className={styles.nft} />
       )}
