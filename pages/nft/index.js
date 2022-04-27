e0b569955d6d5e7683f73701a867653e1b1c05f6
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 15:49:40 +0300

    Update index.js
    Tooltip

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -2,18 +2,34 @@ import { Heading, Img } from "@chakra-ui/react";
 import { Header } from "../../components/Header/Header";
 import { Layout } from "../../components/Layout";
 import styles from "../../styles/Nft.module.css";
+import { Tooltip } from "@chakra-ui/react";
 
 const Nft = () => {
   return (
     <Layout>
       <Heading>NFT</Heading>
       <div className="gallery">
-        <Img src="/nftimages/abstract.jpg" alt="Abstract" width="600" />
-        <Img
-          src="/nftimages/cryptoEagle.jpg"
-          alt="CryptoEagle168"
-          width="600"
-        />
+        <Tooltip
+          label="Abstract"
+          aria-label="A tooltip"
+          closeDelay={500}
+          placement="top"
+        >
+          <Img src="/nftimages/abstract.jpg" alt="Abstract" width="600" />
+        </Tooltip>
+
+        <Tooltip
+          label="CryptoEagle168"
+          aria-label="A tooltip"
+          closeDelay={500}
+          placement="top"
+        >
+          <Img
+            src="/nftimages/cryptoEagle.jpg"
+            alt="CryptoEagle168"
+            width="600"
+          />
+        </Tooltip>
       </div>
     </Layout>
   );
