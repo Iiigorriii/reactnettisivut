44cd60086922375ba05d72987c7f10ce754e27ea
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 14:59:33 +0300

    Update index.js

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -1,13 +1,12 @@
 import { Img } from "@chakra-ui/react";
 import { Layout } from "../../components/Layout";
-import Abstract from "../../misc/nftimages/Abstract.jpg";
 import styles from "../../styles/Nft.module.css";
 
 const Nft = () => {
   return (
     <Layout>
       <h1>testi</h1>
-      <Img src={Abstract} alt="Abstract" />
+      <Img src="/nftimages/abstract.jpg" alt="Abstract" />
       {/* <div class="gallery">
         <Img src="Abstracts.jpg" alt="Abstract" width="600" height="400" />
         <div class="desc"></div> 
