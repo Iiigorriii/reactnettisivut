f5782556c1791c6f3f52890e8e917167ac3c8aec
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-04-27 19:14:10 +0300

    Update index.js

--- pages/nft/index.js
+++ pages/nft/index.js
@@ -65,33 +65,49 @@ const NftPage = () => {
           <Nft src={url} alt={text} />
         ))} */}
 
-        <img src="/nftimages/abstract.jpg" alt="Abstract" />
-        <img src="/nftimages/cryptoEagle.jpg" alt="CryptoEagle168" />
-        <img src="/nftimages/treeApples.jpg" alt="Three apples. Edition 05" />
+        <img
+          src="/nftimages/abstract.jpg"
+          alt="Abstract"
+          className={styles.nft}
+        />
+        <img
+          src="/nftimages/cryptoEagle.jpg"
+          alt="CryptoEagle168"
+          className={styles.nft}
+        />
+        <img
+          src="/nftimages/treeApples.jpg"
+          alt="Three apples. Edition 05"
+          className={styles.nft}
+        />
 
         <video
           src="https://public.nftstatic.com/static/nft/res/08c761d6c95a4e2bb32d18bed4e1d1b1.mp4"
           alt="Satoshi Duck"
           controls
           autoPlay
+          className={styles.nft}
         />
         <video
           src="https://public.nftstatic.com/static/nft/res/b5aca75f909b46c3b45053ba4ed5a9ce.mp4"
           alt="City Duck"
           controls
           autoPlay
+          className={styles.nft}
         />
         <video
           src="https://public.nftstatic.com/static/nft/res/2892eee0726842b6af17307d7d5b5f7b.mp4"
           alt="Grass Duck"
           controls
           autoPlay
+          className={styles.nft}
         />
         <video
           src="https://openseauserdata.com/files/3462a63a03d3cc8ac0f9adc63436c439.mp4"
           alt="Kleoverse Genesis #39"
           controls
           autoPlay
+          className={styles.nft}
         />
       </div>
     </Layout>
