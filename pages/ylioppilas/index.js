4e08b0f4c478f6976dca5670d4495d0fafb8108f
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-06-03 17:39:30 +0300

    Ylioppilaskuva

---
+++ pages/ylioppilas/index.js
@@ -0,0 +1,12 @@
+import { Heading } from "@chakra-ui/react";
+import { Layout } from "../../components/Layout";
+
+const Ylioppilas = () => (
+  <Layout>
+    <Heading>Ylioppilas vm2022</Heading>
+    <br />
+    <img src="/ylioppilasimages/yolakkitesti.jpg" alt="ylioppilaslakkitesti" />
+  </Layout>
+);
+
+export default Ylioppilas;
