282d05f70e2867eb1222d2bb5d00888e96bc5936
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-06-03 20:59:07 +0300

    Coding ylioppilaskuvat

--- pages/ylioppilas/index.js
+++ pages/ylioppilas/index.js
@@ -1,20 +1,80 @@
+// import { Heading } from "@chakra-ui/react";
+// import { Layout } from "../../components/Layout";
+// import styles from "../../styles/Ylioppilas.module.css";
+
+// const Ylioppilas = () => (
+//   <Layout>
+//     <Heading>Ylioppilas vm2022</Heading>
+//     <br />
+//     <img src="/ylioppilasimages/yolakkitesti.jpg" alt="ylioppilaslakkitesti" />
+//     <img src="/ylioppilasimages/yokuva1.jpg" alt="ylioppilaslakkitesti" />
+//     <img src="/ylioppilasimages/yokuva2.jpg" alt="ylioppilaslakkitesti" />
+//   </Layout>
+// );
+
+// export default Ylioppilas;
+
 import { Heading } from "@chakra-ui/react";
 import { Layout } from "../../components/Layout";
+import styles from "../../styles/Ylioppilas.module.css";
+import { Tooltip } from "@chakra-ui/react";
 
-const Ylioppilas = () => (
-  <Layout>
-    <Heading>Ylioppilas vm2022</Heading>
-    <br />
-    <img src="/ylioppilasimages/yolakkitesti.jpg" alt="ylioppilaslakkitesti" />
-    <br />
-    <Heading>Tulokset</Heading>
-    <br />
-    <h1>Äidinkieli M</h1>
-    <h1>Pitkä matematiikka C</h1>
-    <h1>Fysiikka C</h1>
-    <h1>Kemia C</h1>
-    <h1>Englanti b</h1>
-  </Layout>
+const images = [
+  {
+    url: "/ylioppilaskuvat/ylioppilas1.jpeg",
+    text: "KITOS",
+  },
+  {
+    url: "/ylioppilaskuvat/ylioppilas2.jpeg",
+    text: "KITOS",
+  },
+  {
+    url: "/ylioppilaskuvat/ylioppilas3.jpeg",
+    text: "KITOS",
+  },
+];
+
+const videos = [
+  // {
+  //   url: "https://public.nftstatic.com/static/nft/res/08c761d6c95a4e2bb32d18bed4e1d1b1.mp4",
+  //   text: "Satoshi Duck",
+  // },
+];
+
+const Ylioppilas = ({ video, src, alt }) => (
+  <div>
+    <Tooltip label={alt} closeDelay={800} placement="top">
+      {video ? (
+        <video
+          // controls
+          autoPlay
+          loop
+          muted
+          src={src}
+          alt={alt}
+          className={styles.nft}
+        />
+      ) : (
+        <img src={src} alt={alt} className={styles.nft} />
+      )}
+    </Tooltip>
+  </div>
 );
+const YlioppilasPage = () => {
+  return (
+    <Layout>
+      <Heading>Ylioppilas</Heading>
+      <br />
+      <div className={styles.gallery}>
+        {images.map(({ url, text }, index) => (
+          <Ylioppilas key={index} src={url} alt={text} />
+        ))}
 
-export default Ylioppilas;
+        {videos.map(({ url, text }, index) => (
+          <Ylioppilas key={index} src={url} alt={text} video />
+        ))}
+      </div>
+    </Layout>
+  );
+};
+export default YlioppilasPage;
