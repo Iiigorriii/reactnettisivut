8dc7af8b3507c55d3f75284b6d8dd76ee44f2276
veikka-p <83778498+veikka-p@users.noreply.github.com>
2022-06-03 17:45:26 +0300

    Update index.js

--- pages/ylioppilas/index.js
+++ pages/ylioppilas/index.js
@@ -6,6 +6,14 @@ const Ylioppilas = () => (
     <Heading>Ylioppilas vm2022</Heading>
     <br />
     <img src="/ylioppilasimages/yolakkitesti.jpg" alt="ylioppilaslakkitesti" />
+    <br />
+    <Heading>Tulokset</Heading>
+    <br />
+    <h1>Äidinkieli M</h1>
+    <h1>Pitkä matematiikka C</h1>
+    <h1>Fysiikka C</h1>
+    <h1>Kemia C</h1>
+    <h1>Englanti b</h1>
   </Layout>
 );
 
